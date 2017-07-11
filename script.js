b27358728bb527a7d0abd156a3c64d46482913cd
WinnyTroy <winnie.kiragu@savannahinformatics.com>
2017-07-11 10:47:33 +0300

    able to add a new todo to the list

--- script.js
+++ script.js
@@ -5,10 +5,25 @@ var app = angular.module('Todo', []);
 //     in Dependancy Injection in Angular)
 
 app.controller('TodoCtrl', function($scope) {
+    $scope.newTodo = '';
     $scope.todos = [
     'Learn Sketch',
     'Look at Dribbble and feel inferior',
     'Actually learn how to use the Pen tool'
   ];
+
+  $scope.done = function(todo) {
+    var indexOf = $scope.todos.indexOf(todo);
+    if (indexOf !== -1) {
+      $scope.todos.splice(indexOf, 1);
+    }
+  };
+
+  $scope.add = function(e) {
+    if (e.which && e.which === 13) {
+      $scope.todos.push($scope.newTodo);
+      $scope.newTodo = '';
+    }
+  };
 });
 // Controller is set to define state and behaviour of the view
