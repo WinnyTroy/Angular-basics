e20a03262993af9911ab229a0d9c9903add2998c
WinnyTroy <winnie.kiragu@savannahinformatics.com>
2017-07-11 10:19:41 +0300

    added two way data binding to the view. Asynchronously updating the value for the message string on our $scope

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 // initialize the app(creating the module)
 var app = angular.module('Todo', []);
+
+// Empy Array is used to declaring what other modules the app will be using(Aids
+//     in Dependancy Injection in Angular)
+
+app.controller('TodoCtrl', function($scope) {
+    $scope.message = "Hmmmmm...let us see this scocery "
+});
+// Controller is set to define state and behaviour of the view
